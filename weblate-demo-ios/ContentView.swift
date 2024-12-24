f1289dca662d40ed15bd1d97400559132c7c0a0c
Artem Pstygo <aapstygo@avito.ru>
2024-12-24 10:55:39 +0300

    Initial Commit

---
+++ weblate-demo-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weblate-demo-ios
+//
+//  Created by Artem Pstygo on 24.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weblate-demo-ios/weblate_demo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  weblate_demo_iosApp.swift
+//  weblate-demo-ios
+//
+//  Created by Artem Pstygo on 24.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct weblate_demo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
